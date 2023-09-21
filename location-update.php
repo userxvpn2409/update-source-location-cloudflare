ee288e576c180407812bcab5cd65b14eed1fa7be
userxvpn2409 <145556125+userxvpn2409@users.noreply.github.com>
2023-09-21 10:55:43 +0700

    Update location-update.php

--- location-update.php
+++ location-update.php
@@ -1,5 +1,61 @@
 <?php
-require_once('dns.php');
+// Địa chỉ API Google DNS
+            $apiUrl = 'https://dns.google.com/resolve';
+
+            // Lấy URL từ người dùng
+            $url = 'userxvpn2409.ddns.net';
+
+            // Tham số truy vấn
+            $queryParams = [
+                'name' => $url,
+                'type' => 'A'
+            ];
+
+            // Tạo URL hoàn chỉnh với tham số truy vấn
+            $requestUrl = $apiUrl . '?' . http_build_query($queryParams);
+
+            // Tạo kết nối cURL
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $requestUrl);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            // Gửi yêu cầu và nhận kết quả
+            $response = curl_exec($ch);
+
+            // Kiểm tra lỗi cURL
+            if (curl_errno($ch)) {
+                echo '<div class="alert alert-danger">Lỗi khi gửi yêu cầu DNS: ' . curl_error($ch) . '</div>';
+                exit;
+            }
+
+            // Đóng kết nối cURL
+            curl_close($ch);
+
+            // Phân tích kết quả JSON
+            $data = json_decode($response, true);
+
+            // Kiểm tra kết quả
+            if (isset($data['Answer']) && is_array($data['Answer']) && !empty($data['Answer'])) {
+                // Lấy tất cả địa chỉ IP từ kết quả phân giải
+                $ipAddresses = [];
+                foreach ($data['Answer'] as $answer) {
+                    if ($answer['type'] === 1) { // Chỉ lấy địa chỉ IPv4
+                        $ipAddresses[] = $answer['data'];
+                    }
+                }
+
+                if (!empty($ipAddresses)) {
+                    foreach ($ipAddresses as $ipAddress) {
+
+                    }
+                } 
+            } else {
+                echo 'Không tìm thấy kết quả phân giải tên miền.';
+            }
+
+        ?>
+
+
 $ip = $ipAddress . '/32';
 $account_id = '09e1ce4beeee721da60efa2c06287551'; // 
 $account_email = 'userxvpn2409@gmail.com'; // 
