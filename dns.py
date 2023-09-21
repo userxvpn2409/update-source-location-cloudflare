6f2878e964e8b1163527ea04b47f02dd1eb4a315
userxvpn2409 <145556125+userxvpn2409@users.noreply.github.com>
2023-09-21 11:08:10 +0700

    Update dns.py

--- dns.py
+++ dns.py
@@ -26,3 +26,39 @@ if ip_address:
     print(f"The IP address of {domain} is: {ip_address}")
 else:
     print(f"Failed to resolve the IP address of {domain}")
+
+import requests
+
+account_id = '09e1ce4beeee721da60efa2c06287551'
+account_email = 'userxvpn2409@gmail.com'
+api_token = '181af2890ac1bea1646afc0cf12a529fd10d8'
+uuid = 'c792b4647b374c618db91abd2d17d323'
+ip = up_address
+
+url = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/gateway/locations/{uuid}"
+
+headers = {
+    'Authorization': f'Bearer {api_token}',
+    'Content-Type': 'application/json',
+    'X-Auth-Email': account_email,
+    'X-Auth-Key': api_token
+}
+
+payload = {
+    "client_default": True,
+    "ecs_support": True,
+    "name": "RouterZTE",
+    "networks": [
+        {
+            "network": ip
+        }
+    ]
+}
+
+response = requests.put(url, json=payload, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Request failed with status code {response.status_code}")
+    print(response.text)
